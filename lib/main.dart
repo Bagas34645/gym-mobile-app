495086b063ebc06daf4c546a8b062f0901eb0fb1
Diyas <diyasruwandi273@gmail.com>
2026-05-17 12:42:42 +0700

    integrasi firebase dengan flutter

--- lib/main.dart
+++ lib/main.dart
@@ -1,13 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
-import 'package:intl/date_symbol_data_local.dart';
+import 'package:gym_mobile_flutter/firebase_options.dart';
 import 'app/routes/app_pages.dart';
 import 'app/routes/app_routes.dart';
 import 'core/theme/app_theme.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await initializeDateFormatting('id', null);
+
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+
   runApp(
     GetMaterialApp(
       title: "CoreGym",
